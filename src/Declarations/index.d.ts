9f212c42d024497b52eef326607804ad734a5330
danilolucasmd <danilolucasmd@gmail.com>
2021-01-15 12:12:39 -0300

    Changes in type definitions

--- src/Declarations/index.d.ts
+++ src/Declarations/index.d.ts
@@ -1,8 +1,1 @@
-export declare interface SafeVideoPlayerProps {
-  title?: string;
-  progressBarColor?: string;
-  textColor?: string;
-  backgroundColor?: string;
-  onEnterFullscreen?: () => void;
-  onExitFullscreen?: () => void;
-}
+declare module '*.png';
