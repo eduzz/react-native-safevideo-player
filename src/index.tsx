b54721b501357aab4340870c212cc5565464368d
danilolucasmd <danilolucasmd@gmail.com>
2021-01-15 13:43:36 -0300

    Include type declarations to the correct file

--- src/index.tsx
+++ src/index.tsx
@@ -3,3 +3,14 @@ import SafeVideoPlayer from './Components/SafeVideoPlayer';
 export default SafeVideoPlayer;
 
 export * from 'react-native-video';
+
+declare module 'react-native-safevideo-player' {
+  export interface SafeVideoPlayerProps {
+    title?: string;
+    progressBarColor?: string;
+    textColor?: string;
+    backgroundColor?: string;
+    onEnterFullscreen?: () => void;
+    onExitFullscreen?: () => void;
+  }
+}
