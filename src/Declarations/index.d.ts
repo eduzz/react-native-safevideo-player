abb07db90759a36ddf2fb2835aedb09f7555bb7c
danilolucasmd <danilolucasmd@gmail.com>
2021-01-15 13:33:15 -0300

    Changes

--- src/Declarations/index.d.ts
+++ src/Declarations/index.d.ts
@@ -1,12 +1,1 @@
 declare module '*.png';
-
-declare module 'react-native-safevideo-player' {
-  export interface SafeVideoPlayerProps {
-    title?: string;
-    progressBarColor?: string;
-    textColor?: string;
-    backgroundColor?: string;
-    onEnterFullscreen?: () => void;
-    onExitFullscreen?: () => void;
-  }
-}
